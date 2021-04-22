9d7573de177c8cb67d019c21aebf1045150a71e9
Christian Neumann <cneumann@e3code.de>
2021-04-22 13:25:32 +0200

    Fix bounding box calculation.

--- src/components/analysis/index.js
+++ src/components/analysis/index.js
@@ -278,45 +278,30 @@ function getBoundingBox(data) {
     point,
     latitude,
     longitude;
-
-  // Loop through each "feature"
   for (var i = 0; i < data.features.length; i++) {
     coordinates = data.features[i].geometry.coordinates;
+    var geoType = data.features[i].geometry.type;
 
-    if (coordinates.length === 1) {
-      // It's only a single Polygon
-      // For each individual coordinate in this feature's coordinates...
-      for (var j = 0; j < coordinates[0].length; j++) {
-        longitude = coordinates[0][j][0];
-        latitude = coordinates[0][j][1];
-
-        // Update the bounds recursively by comparing the current xMin/xMax and yMin/yMax with the current coordinate
+    if (geoType === 'LineString') {
+      for (var j = 0; j < coordinates.length; j++) {
+        longitude = coordinates[j][0];
+        latitude = coordinates[j][1];
         bounds.xMin = bounds.xMin < longitude ? bounds.xMin : longitude;
         bounds.xMax = bounds.xMax > longitude ? bounds.xMax : longitude;
         bounds.yMin = bounds.yMin < latitude ? bounds.yMin : latitude;
         bounds.yMax = bounds.yMax > latitude ? bounds.yMax : latitude;
       }
-    } else {
-      // It's a MultiPolygon
-      // Loop through each coordinate set
-      for (var j = 0; j < coordinates.length; j++) {
-        // For each individual coordinate in this coordinate set...
-        for (var k = 0; k < coordinates[j][0].length; k++) {
-          longitude = coordinates[j][0][k][0];
-          latitude = coordinates[j][0][k][1];
-
-          // Update the bounds recursively by comparing the current xMin/xMax and yMin/yMax with the current coordinate
+    } else if (geoType === 'Polygon') {
+        for (var j = 0; j < coordinates[0].length; j++) {
+          longitude = coordinates[0][j][0];
+          latitude = coordinates[0][j][1];
           bounds.xMin = bounds.xMin < longitude ? bounds.xMin : longitude;
           bounds.xMax = bounds.xMax > longitude ? bounds.xMax : longitude;
           bounds.yMin = bounds.yMin < latitude ? bounds.yMin : latitude;
           bounds.yMax = bounds.yMax > latitude ? bounds.yMax : latitude;
         }
-      }
     }
   }
-
-  // Returns an object that contains the bounds of this GeoJSON data.
-  // The keys describe a box formed by the northwest (xMin, yMin) and southeast (xMax, yMax) coordinates.
   return bounds;
 }
 
